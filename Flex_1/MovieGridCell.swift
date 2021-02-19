4b82fe75ef734fc70e23be70dfb14d77d467f140
AndreaHabib <andrea.atef@yahoo.com>
2021-02-19 16:37:04 -0500

    DEV: Finished Assignment #2 and finished the first bonus story

---
+++ Flex_1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flex_1
+//
+//  Created by Andrea Habib on 2/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
